8c147b41467fc8db849992d5f3b1a6b0096f6e93
Marcel Stadelmann <m@marceli.to>
2023-05-31 10:43:49 +0200

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 // This is all you.
 import '../css/app.css';
-//import './modules/menu.js';
-import './modules/arguments.js';
+import './modules/menu.js';
+import './modules/cards.js';
 import './modules/masonry.js';
