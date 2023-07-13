9367f251eebdc31bf06928106b78d38262d00287
Marcel Stadelmann <m@marceli.to>
2023-07-13 08:44:10 +0200

    wip

--- resources/js/modules/lazy.js
+++ resources/js/modules/lazy.js
@@ -1,2 +1,8 @@
 import LazyLoad from "vanilla-lazyload";
 let lazyLoadInstance = new LazyLoad();
+// dom content loaded
+document.addEventListener("DOMContentLoaded", function() {
+  // lazy load images
+  lazyLoadInstance.update();
+});
+
